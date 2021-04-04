5d7bbcac2b613faffd8b1c4db12542d6d386443d
natata7 <natata7@gmail.com>
2021-04-04 22:26:48 +0300

    fix: connection db

--- src/controller.js
+++ src/controller.js
@@ -10,7 +10,7 @@ const Redis = require("ioredis");
 const redis = new Redis({
   port: 19458, 
   host: process.env.REDIS_DB,
-  password: `process.env.REDIS_PASS`
+  password: process.env.REDIS_PASS
 });
 
 async function signIn(ctx) {
